2f6d8198fe06c9ad8a7ead15240e60bc048c99f8
Kartik Barmaiya <kartik.barmaiya28@gmail.com>
2023-01-25 18:47:41 +0530

    registration coupon updated

--- pages/register.jsx
+++ pages/register.jsx
@@ -19,12 +19,13 @@ const Register = () => {
     const [city, setCity] = useState('');
     const [state, setState] = useState('');
     const [referral, setReferral] = useState('');
+    const [coupon, setCoupon] = useState('');
+    const [loading, setLoading] = useState(false);
 
-    const[notsubmitted, setNotSubmitted] = useState(true);
 
     const handleSubmit = (e) => {
       e.preventDefault();
-      if(notsubmitted && name && email && org && number && exp && city && state) {
+      if(name && email && org && number && exp && city && state) {
         
         const data = {
           Name: name,
@@ -35,8 +36,10 @@ const Register = () => {
           City: city,
           State: state,
           Referral: referral,
-          Registering_For: bootcamp
+          Registering_For: bootcamp,
+          Coupon:coupon
         }
+        setLoading(true);
 
         axios.post('https://sheet.best/api/sheets/62f3c87d-f9a6-465d-bc16-6d5904f3857e',data).then((response)=>{
           // console.log(response);
@@ -49,7 +52,8 @@ const Register = () => {
           setState("");
           setReferral("");
           setBootcamp("");
-          setNotSubmitted(false);
+          setCoupon("");
+          setLoading(false)
           toast.success("Form submitted successfully!");
           router.push("/");
         })
@@ -77,41 +81,31 @@ const Register = () => {
 
             <div className='my-4 '> 
               <p className='ml-2 mb-2 text-[16px]'>Your Name</p>
-              <input onChange={(e) => setName(e.target.value)} value={name} className=' bg-gray-900 px-6 h-[45px] w-[15rem] md:w-[24rem] border outline-1 outline-blue-200 border-blue-900 rounded-md' />
+              <input required={true} onChange={(e) => setName(e.target.value)} value={name} className=' bg-gray-900 px-6 h-[45px] w-[15rem] md:w-[24rem] border outline-1 outline-blue-200 border-blue-900 rounded-md' />
             </div>
-            {!notsubmitted && !name ? <span className='text-red-600 mb-3'>Please enter your name</span> : null}
 
             <div className='my-4 '>
              <p className='ml-2 mb-2 text-[16px]'>Your Email</p>
              <input onChange={(e) => setEmail(e.target.value)} required={true} value={email} type="email" className=' bg-gray-900 px-6 h-[45px] w-[15rem] md:w-[24rem] border outline-1 outline-blue-200 border-blue-900 rounded-md' />
             </div>
-            {!notsubmitted && !email ? <span className='text-red-600 mb-3'>Please enter your e-mail</span> : null}
         
              <div className='my-4'>
                <p className='ml-2 mb-2 text-[16px]'>Organisation/Institute</p>
                <input required={true} onChange={(e) => setOrg(e.target.value)} value={org} className=' bg-gray-900 px-6 h-[45px] w-[15rem] md:w-[24rem] border outline-1 outline-blue-200 border-blue-900 rounded-md' />
              </div>
-             {!notsubmitted && !org ? <span className='text-red-600 mb-3'>Please enter your organisation</span> : null}
 
             <div className='my-4 '>
               <p className='ml-2 mb-2 text-[16px]'>Phone Number</p>
               <input onChange={(e) => setNumber(e.target.value)} required={true} type="number" value={number} className=' bg-gray-900 px-6 h-[45px] w-[15rem] md:w-[24rem] border outline-1 outline-blue-200 border-blue-900 rounded-md' />
             </div>
-            {!notsubmitted && !number ? <span className='text-red-600 mb-3'>Please enter your contact number</span> : null}
-        
 
-
-        {/* <div className='w-full md:px-28'>
-            <InputField size="large" value={desc} onChange={handleDescChange} placeholder='Description' rows={4} />
-        </div> */}
-
-  
             <div className='my-4 '>
                 <p className='ml-2 mb-2 text-[16px]'>Your experience level</p>
                 <select
                   defaultValue="Experience Level"
                   onChange={(e) => setExp(e.target.value)}
                   value={exp}
+                  required={true}
                   placeholder="Experiance"
                   className=" bg-gray-900 px-6 h-[45px] w-[15rem] md:w-[24rem] border outline-1 outline-blue-200 border-blue-900 rounded-md"
                 >
@@ -120,20 +114,17 @@ const Register = () => {
                   <option value="Expert">Expert</option>
                 </select>
             </div>
-            {/* {submitted && !exp ? <span className='text-red-600 mb-3'>Please enter your experience level</span> : null} */}
 
             <div className='my-4 '>
                 <p className='ml-2 mb-2 text-[16px]'>City</p>
                 <input onChange={(e) => setCity(e.target.value)} value={city} className=' bg-gray-900 px-6 h-[45px] w-[15rem] md:w-[24rem] border outline-1 outline-blue-200 border-blue-900 rounded-md' />
             </div>
-            {!notsubmitted && !city ? <span className='text-red-600 mb-3'>Please enter your city</span> : null}
     
 
           <div className='my-4 '>
             <p className='ml-2 mb-2 text-[16px]'>State</p>
             <input onChange={(e) => setState(e.target.value)} value={state} className=' bg-gray-900 px-6 h-[45px] w-[15rem] md:w-[24rem] border outline-1 outline-blue-200 border-blue-900 rounded-md' />
           </div>
-          {!notsubmitted && !state ? <span className='text-red-600 mb-3'>Please enter your state</span> : null}
 
           <div className='my-4 '>
             <p className='ml-2 mb-2 text-[16px]'>Referral</p>
@@ -141,8 +132,14 @@ const Register = () => {
           </div>
 
           <div className='my-4 '>
+            <p className='ml-2 mb-2 text-[16px]'>Coupon code</p>
+            <input onChange={(e) => setCoupon(e.target.value)} value={coupon} className=' bg-gray-900 px-6 h-[45px] w-[15rem] md:w-[24rem] border outline-1 outline-blue-200 border-blue-900 rounded-md' />
+          </div>
+
+          <div className='my-4 '>
                 <p className='ml-2 mb-2 text-[16px]'>Registering for bootcamp:</p>
                 <select
+                  required={true}
                   defaultValue="Experience Level"
                   onChange={(e) => setBootcamp(e.target.value)}
                   value={bootcamp}
@@ -157,7 +154,7 @@ const Register = () => {
             </div>
        
 
-        <button type="submit" className={`border-2 w-[12rem] mr-4 border-[#003979] font-semibold rounded-full px-12 py-3 mt-7 inline-block hover:bg-[#1B2430] hover:text-white`}>SUBMIT</button>
+        <button type="submit" className={`border-2 w-[12rem] mr-4 border-[#003979] font-semibold rounded-full px-12 py-3 mt-7 inline-block hover:bg-[#1B2430] hover:text-white`}>{loading? "Loading..." : "SUBMIT"}</button>
         </form>
         </div>
 
